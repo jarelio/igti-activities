1b423c016706be2254e2f79126ee6b82365ba1f5
jarelio <jarelio_gomes@atlantico.com.br>
2020-09-01 23:33:47 -0300

    modify cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: 'https://igti-module04-frontend.herokuapp.com/',
+        origin: 'https://igti-module04-frontend.herokuapp.com',
     })
 );
 
